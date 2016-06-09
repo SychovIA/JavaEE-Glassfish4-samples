bd91713d45b0841cee31d8374b45284182b1e38c
Lenovo <Lenovo@Lenovo-PC>
2016-06-09 15:31:06 +0300

    first commit

---
+++ websocketchat/src/com/demo/websocketchat/named/User.java
@@ -0,0 +1,19 @@
+package com.demo.websocketchat.named;
+
+import javax.enterprise.inject.Model;
+
+
+@Model
+public class User {
+
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+}
